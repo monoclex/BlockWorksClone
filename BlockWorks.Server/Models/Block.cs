a63a68cf7219ae5a744cdec153625c683f30cfcb
SirJosh3917 <joshuaneedham@hotmail.com>
2019-02-28 21:11:56 -0500

    minor "improvements"
    - break the server
    - i don't plan on fixing it
    - use early version of Decorator.IO to create a models file
    - use robots to do style cleaning of the code

---
+++ BlockWorks.Server/Models/Block.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using PlayerIO.GameLibrary;
+
+namespace BlockWorks.Server.Models.Extensions
+{
+	public static class BlockExtensions
+	{
+		public const string Type = "b";
+
+		public static uint GetX(this IBlock block)
+		{
+			return UtilityFunctions.GetX(block.Position);
+		}
+
+		public static void SetX(this IBlock block, uint newX)
+		{
+			block.Position = UtilityFunctions.ToBlocks(newX, UtilityFunctions.GetY(block.Position));
+		}
+
+		public static uint GetY(this IBlock block)
+		{
+			return UtilityFunctions.GetY(block.Position);
+		}
+
+		public static void SetY(this IBlock block, uint newY)
+		{
+			block.Position = UtilityFunctions.ToBlocks(UtilityFunctions.GetX(block.Position), newY);
+		}
+
+		public static Message ToMessage(this IBlock block, bool sending)
+		{
+			var serializedArray = block.Serialize();
+
+			var modified =
+				sending
+					? serializedArray.Skip(1)
+					: serializedArray;
+
+			var array = modified.Concat(new object[] {0u}).ToArray();
+
+			return Message.Create(Type, array);
+		}
+	}
+}
---
+++ BlockWorks.Server/Models/Fly.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using PlayerIO.GameLibrary;
+
+namespace BlockWorks.Server.Models.Extensions
+{
+	public static class FlyExtensions
+	{
+		public const string Type = "fly";
+
+
+		public static Message ToMessage(this IFly fly, bool sending)
+		{
+			var serializedArray = fly.Serialize();
+
+			var modified =
+				sending
+					? serializedArray.Skip(1)
+					: serializedArray;
+
+			var array = modified.Concat(new object[] {0u}).ToArray();
+
+			return Message.Create(Type, array);
+		}
+	}
+
+	/*
+		public Message Pack(Target pet)
+		{
+			return pet == Target.Sending
+				? Message.Create(Type, Id, FlyState)
+				: Message.Create(Type, FlyState);
+		}
+	*/
+}
